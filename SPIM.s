7d002202080087977d77f7f9c0fb53141c8a006a
gudwns6199 <90139570+gudwns6199@users.noreply.github.com>
2021-09-09 10:01:16 +0900

    Update SPIM.s

--- SPIM.s
+++ SPIM.s
@@ -1,7 +1,5 @@
 .text
 .globl main
 main:
-  addi $t1, $0, 0x7FFFFFFF # later, in more detail
-  add $t2, $t1, $t1 # overflow
-  addu $t3, $t1, $t1 # no exception
-  addiu $t4, $t1, -1 # negative constant for addiu
+  addi $t1, $0, -2
+  subu $t2, $0, $t1
