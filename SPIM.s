abcbfc11168af8ead190f6232ed1181590fb4c7a
gudwns6199 <90139570+gudwns6199@users.noreply.github.com>
2021-09-09 09:32:39 +0900

    Update SPIM.s

--- SPIM.s
+++ SPIM.s
@@ -1,6 +1,9 @@
 .text
 .globl main
 main:
-  addi $t0, $0, -2
-  addi $t1, $0, 0xffffffff
-  addi $t2, $0, -1
+  addi $t3, $0, 7
+  addi $t4, $0, 6
+  addi $t5, $t3, $t4
+  addi $t6, $t3, $t4
+  addi $t7, $0, -6
+  addi $t8, $t3, %t7
