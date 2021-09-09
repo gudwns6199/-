00c8458bca24f9cccb041f7f8701b0804c00af72
gudwns6199 <90139570+gudwns6199@users.noreply.github.com>
2021-09-09 10:23:56 +0900

    Update SPIM.s

--- SPIM.s
+++ SPIM.s
@@ -1,5 +1,8 @@
 .text
 .globl main
 main:
-  addi $t1, $0, -2
-  subu $t2, $0, $t1
+  addi $t0, $0, 2 # $8 gets 2
+  addi $t1, $0, 3 # $9 gets 3
+  mult $t0, $t1
+  mflo $t2 # $10 gets 2x3
+  mfhi $t3
