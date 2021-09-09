7b5cdca67748fb3f85ad0780ac0496a4a7828285
gudwns6199 <90139570+gudwns6199@users.noreply.github.com>
2021-09-09 09:34:26 +0900

    Update SPIM.s

--- SPIM.s
+++ SPIM.s
@@ -3,7 +3,7 @@
 main:
   addi $t3, $0, 7
   addi $t4, $0, 6
-  addi $t5, $t3, $t4
-  addi $t6, $t3, $t4
+  add $t5, $t3, $t4
+  sub $t6, $t3, $t4
   addi $t7, $0, -6
-  addi $t8, $t3, %t7
+  add $t8, $t3, %t7
